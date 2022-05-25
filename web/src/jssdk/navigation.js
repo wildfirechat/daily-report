4eb79373844ee7b19c233518fedb6c50f88d5bfc
x86 <1960867973@qq.com>
2022-05-25 11:55:41 +0800

    update close

--- web/src/jssdk/navigation.js
+++ web/src/jssdk/navigation.js
@@ -4,6 +4,7 @@ const bridge = window.__wf_bridge_ ? window.__wf_bridge_ : require('dsbridge');
 
 export default class Navigation {
     close(successCB, failCB) {
-        bridge.call('close', _handleNativeCall(successCB, failCB));
+        // call 方法的声明：  call (handlerName: string, args?: any, responseCallback?: (retValue: any) => void): any;
+        bridge.call('close', {}, _handleNativeCall(successCB, failCB));
     }
 }
