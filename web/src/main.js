d7b99ab839f828335cf01545705e36e1312753f9
imndx <imndxx@gmail.com>
2025-09-28 15:15:46 +0800

    add vconsole

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './pages/ReportListPage'
+// import VConsole from 'vconsole';
 
 Vue.config.productionTip = false
+
+// 开发调试时，可打开下面的 vConsole，方便查看日志
+// const vConsole = new VConsole();
 
 new Vue({
   render: h => h(App),
