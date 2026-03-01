4143304084e323c4bde862f34011d418cd53efe1
imndx <imndxx@gmail.com>
2026-03-01 17:44:11 +0800

    修复非 web 端，加载工作台失败

--- web/src/jssdk/bridgeClientImpl.uni.js
+++ web/src/jssdk/bridgeClientImpl.uni.js
@@ -2,8 +2,12 @@
  * 大概流程
  *  采用类 client-server 模式实现开放平台 js sdk，有两部分相关代码：
  * 1. client 运行在工作台所加载的 webview 里面
- * 2. server 直接运行在工作台页面里面，也就是主窗口的渲染进程
- * 3. client 和 server 之间的交互，通过 websocket 进行中转
+ * 2. server 直接运行在工作台页面里面，PC端是主窗口的渲染进程
+ * 3. client 和 server 之间的交互：
+ *   1. PC 端，通过 websocket 通信
+ *   2. Web 端，通过 window.postMessage 通信，未实现
+ *   3. 原生移动端，包括 iOS、 Android 和 鸿蒙NEXT，采用 dsbridge 实现，深层的原理是各自平台的 js 原生交互方案，比如Android 是采用 @JavascriptInterface 及 evaluateJavascript 实现
+ *   4. uniapp，通过 uni.postMessage 和 _webView.evalJs 实现
  *
  */
 let callbackMap = new Map();
@@ -70,18 +74,20 @@ function register(handlerName, callback) {
     eventListeners[handlerName] = callback;
 }
 
-console.log('add UniAppJSBridgeReady listener')
-document.addEventListener('UniAppJSBridgeReady', () => {
-    uniappBridgeReady = true;
-    console.log('receive UniAppJSBridgeReady event')
-    uni.getEnv((res) => {
-        console.log('当前环境：' + JSON.stringify(res));
-        // web 端 h5: true
-        // if (res.nvue){
-        //     console.log('init uni client')
-        //     init();
-        // }else {
-        //     console.log('not init uni client')
-        // }
+if (navigator.userAgent.indexOf('uni-app') >= 0) {
+    console.log('add UniAppJSBridgeReady listener')
+    document.addEventListener('UniAppJSBridgeReady', () => {
+        uniappBridgeReady = true;
+        console.log('receive UniAppJSBridgeReady event')
+        uni.getEnv((res) => {
+            console.log('当前环境：' + JSON.stringify(res));
+            // web 端 h5: true
+            // if (res.nvue){
+            //     console.log('init uni client')
+            //     init();
+            // }else {
+            //     console.log('not init uni client')
+            // }
+        });
     });
-});
+}
